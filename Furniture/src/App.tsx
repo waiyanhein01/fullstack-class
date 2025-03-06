b14f7f2174b0597b4bd4e205eef17cf100beacb7
waiyanhein01 <wyan00196@gmail.com>
2025-03-06 18:28:38 +0630

    feat:project-setup

---
+++ Furniture/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-svh">
+    <Button>Click me</Button>
+  </div>
+  )
+}
+
+export default App
