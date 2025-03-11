ea42353e92922174bb87161fbb98a22571504296
waiyanhein01 <wyan00196@gmail.com>
2025-03-11 13:04:27 +0630

    fix:dark-mode

--- Furniture/src/components/layout/components/Header.tsx
+++ Furniture/src/components/layout/components/Header.tsx
@@ -5,11 +5,11 @@ import { ModeToggle } from "@/components/mode-toggle";
 
 const Header = () => {
   return (
-    <header className="flex items-center border-b border-b-slate-300 h-16">
+    <header className="flex items-center border-b border-b-slate-300 h-16 bg-red-300">
       <MainNavBar items={siteConfig.mainNav} />
       <MobileNavBar items={siteConfig.mainNav} />
 
-      <div className=" pe-8">
+      <div className=" bg-amber-300 flex flex-1 justify-end pe-8">
         <ModeToggle />
       </div>
     </header>
