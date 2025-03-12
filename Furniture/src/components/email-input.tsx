286e217e2263163b761f14b2accb7b8187e2a653
waiyanhein01 <wyan00196@gmail.com>
2025-03-13 00:14:20 +0630

    fix:footer-finished

---
+++ Furniture/src/components/email-input.tsx
@@ -0,0 +1,63 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Icons } from "./Icons";
+
+const EmailSchema = z.object({
+  email: z.string({
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const EmailInput = () => {
+  const form = useForm<z.infer<typeof EmailSchema>>({
+    resolver: zodResolver(EmailSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof EmailSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem >
+              <div className="border border-slate-300 rounded-md flex p-1">
+              <FormLabel className="sr-only">Username</FormLabel>
+              <FormControl className="">
+                <Input placeholder="example@gmail.com" {...field} className="" />
+              </FormControl>
+
+              <Button size={"icon"} className="" aria-hidden="true">
+                <Icons.SendIcon aria-hidden="true"/>
+              </Button>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default EmailInput;
--- Furniture/src/components/layout/components/Footer.tsx
+++ Furniture/src/components/layout/components/Footer.tsx
@@ -1,16 +1,41 @@
+import EmailInput from "@/components/email-input";
 import { Icons } from "@/components/Icons";
 import { siteConfig } from "@/configs/site";
 import { Link } from "react-router";
 const Footer = () => {
   return (
-    <footer className=" flex items-center mt-auto border border-t-slate-300 h-16">
-      <section className="container mx-auto">
-        <div className="">
+    <footer className=" flex items-center mt-auto border-t border-t-slate-300">
+      <section className="container mx-auto py-10">
+        <div className="flex flex-col md:flex-row md:items-start justify-between">
           <Link to={"/"} className="flex items-center gap-3">
             <Icons.NavIcon className="size-7" aria-hidden="true" />
             <h1 className="inline-block font-semibold">{siteConfig.name}</h1>
             <span className="sr-only">Home</span>
           </Link>
+
+          <div className="">
+            <div className="grid grid-cols-2 md:grid-cols-4 space-x-10 md:space-x-20 lg:space-x-24">
+              {siteConfig.footerNav.map((head) => (
+                <h1 key={head.title} className="text-base font-semibold pb-4">
+                  {head.title}
+                </h1>
+              ))}
+              {siteConfig.footerNav.map((list) => (
+                <ul key={list.title}>
+                  {list.items.map((item) => (
+                    <li key={item.title} className="text-muted-foreground text-sm pb-2 hover:text-foreground cursor-pointer">
+                      <Link to={String(item.href)} target={item.external ? "_blank" : ""}>{item.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              ))}
+            </div>
+          </div>
+
+          <div className="lg:w-[250px] w-full">
+            <h1 className="text-base font-semibold pb-4">Subscribe to our newsletter.</h1>
+            <EmailInput/>
+          </div>
         </div>
       </section>
     </footer>
