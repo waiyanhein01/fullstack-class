be273d3a132a7103947deaca439b4cb3f1af5fd4
waiyanhein01 <wyan00196@gmail.com>
2025-03-14 23:05:30 +0630

    fix:Blog card issues

--- Furniture/src/features/home/components/BlogCardSection.tsx
+++ Furniture/src/features/home/components/BlogCardSection.tsx
@@ -10,13 +10,13 @@ const BlogCardSection = ({ posts }: BlogCardSectionProps) => {
   return (
     <section className="mt-24">
       <Title title="Recent Blog" href={`/blogs`} linkTitle="View All Posts" />
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 mt-6">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 mt-6">
         {posts.map((post) => (
           <Link key={post.id} to={`/blogs/${post.id}`} className="">
-            <img src={post.image} alt="Blog Image" className="w-full rounded-md" />
-            <h1 className="text-base font-semibold line-clamp-1 my-2">{post.title}</h1>
-            <span className="text-sm">
-              by<span className="font-semibold"> {post.author} </span>on
+            <img src={post.image} alt="Blog Image" className="w-full rounded-2xl" />
+            <h1 className="ml-4 text-base font-semibold line-clamp-1 my-2">{post.title}</h1>
+            <span className="ml-4 text-sm">
+              by<span className=" font-semibold"> {post.author} </span>on
               <span className="font-semibold"> {post.updated_at}</span>
             </span>
           </Link>
