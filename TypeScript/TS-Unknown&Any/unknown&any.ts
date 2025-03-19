99f153b4cf029e86323f20c3c342eb01c0093da8
waiyanhein01 <wyan00196@gmail.com>
2025-03-19 12:43:18 +0630

    fix:learning TypeScript

---
+++ TypeScript/TS-Unknown&Any/unknown&any.ts
@@ -0,0 +1,17 @@
+// let unData : unknown = "hello"; ရေးထုံးနဲ့ unknown Type တွေကို သတ်မှတ် လို့ရပါတယ် ။
+
+// let anyData : any = "hello"; ရေးထုံးနဲ့ any Type တွေကို သတ်မှတ် လို့ရပါတယ် ။
+
+//  unknown နဲ့ any type က Priority Level  အမြင့်ဆုံးတွေဖြစ်ပါတယ် ။
+//  ဒီ Type တွေကို သတ်မှတ်ထားတဲ့အခါ အခြားတန်ဖိုးတွေအကုန် လက်ခံနိုင်ပါတယ် ။
+
+// Any Type
+
+// ဘယ်လို Data type မဆို ကိုင်တွယ်နိုင်တဲ့ Flexible Type ဖြစ်ပါတယ် ။
+// Strict Type-Checking ကို Disable လုပ်နိုင်စွမ်းရှိပါတယ် ။
+// any Type ကို တတ်နိုင်သလောက် ရှောင်ပြီး သုံးသင့်ပါတယ် ။
+// သို့ပေ့မယ် လည်း Legacy JavaScript Code တွေကို Migrate လုပ်တဲ့အခါ ၊ API response ကို Handle လုပ်တဲ့အခါမှာ any Type ကို အသုံးပြုလေ့ ရှိကြပါတယ် ။
+
+// Unknown Type
+// TypeScript မှာ unknown ကို any အစား Type-Safe အနေနဲ့ သုံးကြပါတယ် ။
+// သူလည်းပဲ အခြား Type အားလုံးကို Assign လုပ်လို့ရပါတယ် ။ သို့ပေ့မယ် any နဲ့ မတူတာက method properties တွေကို တိုက်ရိုက် ယူသုံးလို့ မရတာပဲဖြစ်ပါတယ် ။
