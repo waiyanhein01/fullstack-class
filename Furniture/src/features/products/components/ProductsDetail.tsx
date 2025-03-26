b1e3ffd09cb1a2662cc9514aa6bf53482763b248
waiyanhein01 <wyan00196@gmail.com>
2025-03-26 16:23:18 +0630

    feat:products detail

--- Furniture/src/features/products/components/ProductsDetail.tsx
+++ Furniture/src/features/products/components/ProductsDetail.tsx
@@ -1,14 +1,31 @@
 import { BreadCrumb } from "@/components/layout/components/BreadCrumb";
 import Container from "@/components/layout/components/Container";
+import { products } from "@/data/products";
+import ProductsCard from "./ProductsCard";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const ProductsDetail = () => {
   return (
-    <div className="container mx-auto my-20">
+    <div className="container mx-auto my-20 overflow-hidden">
       <Container>
         <BreadCrumb
           currentPage="ProductsDetail"
           links={[{ path: "/products", title: "Products" }]}
         />
+        <div className="">For products Details</div>
+        <div className="mt-8">
+          <h1 className="pb-4 text-xl font-bold 2xl:text-2xl">
+            More Products from Furniture Shop
+          </h1>
+          <ScrollArea className="w-96 rounded-md whitespace-nowrap lg:w-full">
+            <div className="flex w-max gap-8 p-4 lg:w-full lg:p-0">
+              {products.slice(0, 4).map((product) => (
+                <ProductsCard product={product} key={product.id} />
+              ))}
+            </div>
+            <ScrollBar orientation="horizontal" />
+          </ScrollArea>
+        </div>
       </Container>
     </div>
   );
