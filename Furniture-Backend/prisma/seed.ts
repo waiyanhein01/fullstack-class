58f51aa386965b2cb22963b019c6b83c660d568f
waiyanhein01 <wyan00196@gmail.com>
2025-05-08 16:50:49 +0630

    learn:prisma

---
+++ Furniture-Backend/prisma/seed.ts
@@ -0,0 +1,74 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient, Prisma } from "../generated/prisma";
+import * as bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+// const userData: Prisma.UserCreateInput[] = [
+//   {
+//     phone: "1234567890",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+//   {
+//     phone: "1234567891",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+//   {
+//     phone: "1234567892",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+//   {
+//     phone: "1234567893",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+//   {
+//     phone: "1234567894",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+//   {
+//     phone: "1234567895",
+//     password: "",
+//     randToken: "hasfpwehsdvnuru490dnpsf39u48n3u4",
+//   },
+// ];
+
+function createRandomUser() {
+  return {
+    phone: faker.phone.number({ style: "international" }),
+    password: "",
+    randToken: faker.internet.jwt(),
+  };
+}
+
+export const userData = faker.helpers.multiple(createRandomUser, {
+  count: 5,
+});
+
+async function main() {
+  console.log("Seeding started");
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash("123456", salt);
+
+  userData.forEach(async (user) => {
+    user.password = password;
+    await prisma.user.create({
+      data: user,
+    });
+  });
+  console.log("Seeding finished");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
