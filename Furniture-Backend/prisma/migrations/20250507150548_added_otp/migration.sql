5859e2449d5668eaeeebab5ee24530e2a27668b6
waiyanhein01 <wyan00196@gmail.com>
2025-05-08 10:20:49 +0630

    learn:prisma

---
+++ Furniture-Backend/prisma/migrations/20250507150548_added_otp/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Otp" (
+    "id" SERIAL NOT NULL,
+    "phone" VARCHAR(15) NOT NULL,
+    "otp" VARCHAR(6) NOT NULL,
+    "rememberToken" TEXT NOT NULL,
+    "verifyToken" TEXT,
+    "count" SMALLINT NOT NULL DEFAULT 0,
+    "error" SMALLINT NOT NULL DEFAULT 0,
+    "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("id")
+);
