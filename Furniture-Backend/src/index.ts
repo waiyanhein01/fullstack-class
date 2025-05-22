e27d759af87c16f4089616b7dacf733dd5f53a04
waiyanhein01 <wyan00196@gmail.com>
2025-05-22 19:59:09 +0630

    fix : issues

--- Furniture-Backend/src/index.ts
+++ Furniture-Backend/src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { app } from "./app";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () =>
   console.log(`Server is running on port: http://localhost:${PORT}`)
