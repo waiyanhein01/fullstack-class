89d0761db5f080223a901d640b3bf6be20ad7534
waiyanhein01 <wyan00196@gmail.com>
2025-06-16 16:46:39 +0630

    learn : create post api

--- Furniture-Backend/src/controllers/admin/postController.ts
+++ Furniture-Backend/src/controllers/admin/postController.ts
@@ -1,27 +1,82 @@
 import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
 import { errorCode } from "../../../config/errorCode";
+import { createError } from "../../utils/errorUtil";
+import { getUserById } from "../../services/authService";
+import { checkUserIfNotExist } from "../../utils/authUtil";
+import { checkImageFromMulterSupport } from "../../utils/checkUtil";
+import { createOnePost, PostArg } from "../../services/createPostService";
+import ImageQueue from "../../jobs/queues/imageQueue";
 
 interface UserIdRequest extends Request {
   userId?: number;
 }
 
 export const createPost = [
-  body("lng", "Language code is invalid.")
-    .trim()
-    .notEmpty()
-    .matches("^[a-z]+$")
-    .isLength({ min: 2, max: 3 }),
+  body("title", "Title is required.").trim().notEmpty().escape(),
+  body("content", "Content is required.").trim().notEmpty().escape(),
+  body("body", "Body is required.").trim().notEmpty().escape(),
+  body("category", "Category is required.").trim().notEmpty().escape(),
+  body("type", "Type is required.").trim().notEmpty().escape(),
+  body("tags", "Tags is invalid")
+    .optional({ nullable: true })
+    .customSanitizer((value) => {
+      if (value) {
+        return value.split(",").filter((tag: string) => tag.trim() !== "");
+      }
+      return value;
+    }),
+
   async (req: UserIdRequest, res: Response, next: NextFunction) => {
     const errors = validationResult(req).array({ onlyFirstError: true });
     if (errors.length > 0) {
-      const error: any = new Error(errors[0].msg);
-      error.status = 400;
-      error.errorCode = errorCode.invalid;
-      return next(error);
+      return next(createError(errors[0].msg, 400, errorCode.invalid));
     }
 
-    res.status(200).json({ message: "Post created successfully." });
+    const { title, content, body, category, type, tags } = req.body;
+    const userId = req.userId;
+    const user = await getUserById(userId!);
+    checkUserIfNotExist(user);
+
+    const image = req.file;
+    checkImageFromMulterSupport(image);
+
+    const splitFileName = req.file?.filename.split(".")[0];
+
+    await ImageQueue.add(
+      "optimize-image",
+      {
+        filePath: req.file?.path,
+        fileName: `${splitFileName}.webp`,
+        width: 835,
+        height: 577,
+        quality: 100,
+      },
+      {
+        attempts: 3,
+        backoff: {
+          type: "exponential",
+          delay: 1000,
+        },
+      }
+    );
+
+    const data: PostArg = {
+      title,
+      content,
+      body,
+      image: req.file!.filename,
+      authorId: user!.id,
+      category,
+      type,
+      tags,
+    };
+
+    const post = await createOnePost(data);
+
+    res
+      .status(200)
+      .json({ message: "A new post created successfully.", postId: post.id });
   },
 ];
 
---
+++ Furniture-Backend/src/services/createPostService.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+export interface PostArg {
+  title: string;
+  content: string;
+  body: string;
+  image: string;
+  authorId: number;
+  category: string;
+  type: string;
+  tags: string[];
+}
+
+export const createOnePost = async (postData: PostArg) => {
+  let data: any = {
+    title: postData.title,
+    content: postData.content,
+    body: postData.body,
+    image: postData.image,
+    author: {
+      connect: {
+        id: postData.authorId,
+      },
+    },
+    category: {
+      connectOrCreate: {
+        where: {
+          name: postData.category,
+        },
+        create: {
+          name: postData.category,
+        },
+      },
+    },
+    type: {
+      connectOrCreate: {
+        where: {
+          name: postData.type,
+        },
+        create: {
+          name: postData.type,
+        },
+      },
+    },
+  };
+
+  if (postData.tags && postData.tags.length > 0) {
+    data.tags = {
+      connectOrCreate: postData.tags.map((tag) => ({
+        where: {
+          name: tag,
+        },
+        create: {
+          name: tag,
+        },
+      })),
+    };
+  }
+  return prisma.post.create({ data });
+};
