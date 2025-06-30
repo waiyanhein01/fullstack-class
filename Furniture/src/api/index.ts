1c4999d38ce8626d09b8943e6bc8b9d4a45c8d60
waiyanhein01 <wyan00196@gmail.com>
2025-06-30 12:22:11 +0630

    learn : set up axios data fetching

---
+++ Furniture/src/api/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+// for show without login and register web site or app then click something need login
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Handle unauthorized access, e.g., redirect to login
+      window.location.href = `/login?redirect=${encodeURIComponent(window.location.pathname)}`;
+    }
+    console.error("API error:", error);
+    return Promise.reject(error);
+  },
+);
+
+// for auth
+export const authApi = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export default api;
