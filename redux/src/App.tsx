f5390e0eff6625a3bf51c76bb08ee29a35852d61
waiyanhein01 <wyan00196@gmail.com>
2026-03-17 14:53:29 +0630

    learn : 580-583. Dispatch & selectors

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,12 +1,29 @@
 import { Button } from "@/components/ui/button";
-import { useAppSelector } from "./hooks/useRedux";
+import { useAppDispatch, useAppSelector } from "./hooks/useRedux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  adultUsers,
+} from "./store/counterSlice";
 
 function App() {
-  const count = useAppSelector((state) => state.counter.value);
+  const count = useAppSelector((state) => state.counter.value); // approach 1
+  // const count = useAppSelector(counter); // approach 2
+
+  const adultUsersLength = useAppSelector(adultUsers);
+  const dispatch = useAppDispatch();
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-3">{count}</h1>
-      <Button>Click me</Button>
+      <Button onClick={() => dispatch(increment())}>Increment</Button>
+      <Button onClick={() => dispatch(decrement())}>Decrement</Button>
+      <Button onClick={() => dispatch(incrementByAmount(5))}>
+        Increment by 5
+      </Button>
+
+      <h2 className="text-2xl font-bold mt-5">Adult Users</h2>
+      <ul>{adultUsersLength}</ul>
     </div>
   );
 }
--- redux/src/store/counterSlice.ts
+++ redux/src/store/counterSlice.ts
@@ -1,20 +1,46 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from ".";
 
-interface CounterSliceState {
-    value: number
+interface User {
+    id: number,
+    name: string,
+    age: number
 }
 
-const initialState: CounterSliceState = { value: 0 }
+interface CounterSliceState {
+    value: number,
+    users: User[]
+}
+
+const initialState: CounterSliceState = {
+    value: 0, users: [
+        { id: 1, name: "Alice", age: 30 },
+        { id: 2, name: "Bob", age: 16 },
+        { id: 3, name: "Charlie", age: 35 }
+    ]
+}
 
 const counterSlice = createSlice({
     name: "counter", // unique identifying the slice
     initialState,
+
+    // reducers are the functions that handle actions and update the state(write data)
     reducers: {
         increment: (state) => { state.value += 1 },
         decrement: (state) => { state.value -= 1 },
         incrementByAmount: (state, action: PayloadAction<number>) => { state.value += action.payload }
+    },
+
+    // selectors are functions that extract specific pieces of state(read data)
+    selectors: {
+        adultUsers: (state) => {
+            const newUsers = state.users.filter(user => user.age >= 18)
+            return newUsers.length
+        }
     }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions // approach 1
+export const counter = (state: RootState) => state.counter.value // approach 2
+export const { adultUsers } = counterSlice.selectors // for learn selectors
 export default counterSlice.reducer
