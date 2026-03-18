9c98e4db5dad0cca8e8af9dc14fc1af478b4d882
waiyanhein01 <wyan00196@gmail.com>
2026-03-18 16:39:47 +0630

    learn : 584-588. createAsyncThunk

--- redux/src/features/posts/components/Posts.tsx
+++ redux/src/features/posts/components/Posts.tsx
@@ -1,10 +1,33 @@
-import { useAppDispatch } from "@/hooks/useRedux";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { fetchPosts } from "@/store/postsSlice";
+import { useEffect } from "react";
 
 function Posts() {
   const dispatch = useAppDispatch();
+  const { items, status, error } = useAppSelector((state) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="">All Posts</h1>
+      <h1 className="mb-4">All Posts</h1>
+      {status === "failed" && <p className="text-red-500">{error}</p>}
+      {status === "pending" && <p>Loading...</p>}
+      {status === "succeeded" && (
+        <ul>
+          {items.map((post) => (
+            <li
+              key={post.id}
+              className="border border-gray-300 rounded-md p-4 w-full max-w-md mb-3"
+            >
+              <h2 className="text-xl font-bold">{post.title}</h2>
+              <p>Author: {post.author}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- redux/src/store/postsSlice.ts
+++ redux/src/store/postsSlice.ts
@@ -1,18 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// interface PostsSliceState {
-//     id: number,
-//     title: string,
-//     author: string
-// }
-// const initialState = {
+const BASE_API_URL = "http://localhost:4000/posts"
 
-// }
+interface Post {
+    id: number,
+    title: string,
+    author: string
+}
+
+interface PostsSliceState {
+    items: Post[],
+    status: "idle" | "pending" | "succeeded" | "failed",
+    error: string | null
+}
+
+const initialState: PostsSliceState = {
+    items: [],
+    status: "idle",
+    error: null
+}
+
+// dispatch(fetchPosts()) // type "posts/fetchPosts"
+export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
+    // call api
+    const response = await axios.get(BASE_API_URL)
+    return response.data
+})
+
+// 1. "posts/fetchPosts/pending"
+// OR 
+// "posts/fetchPosts/fulfilled", payload: response.data
+// OR 
+// "posts/fetchPosts/rejected", payload: response.data
 
 const PostsSlice = createSlice({
     name: "posts",
-    initialState: {},
-    reducers: {}
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchPosts.pending, (state) => {
+            state.status = "pending"
+            state.error = null
+        })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = "succeeded"
+                state.items.push(...action.payload)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = "failed"
+                state.error = action.error.message || "Failed to fetch posts"
+            })
+    }
 })
 
 export default PostsSlice.reducer
